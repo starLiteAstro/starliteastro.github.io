a8365f16ec7ba81d0142cfcfda2e5400cf78fbea
starliteastro <70979587+starLiteAstro@users.noreply.github.com>
2025-09-20 04:59:59 +0100

    Static fixes

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,7 @@
 import { MetadataRoute } from "next"
 
+export const dynamic = 'force-static'
+
 export const baseUrl = 'https://starliteastro.github.io'
 
 export default function sitemap(): MetadataRoute.Sitemap {
